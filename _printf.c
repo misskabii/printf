8acfc88adb55157700ce21285f03e6b246be83d0
misskabii <shazmakesh254@gmail.com>
2023-03-28 01:00:19 +0300

    task 0

--- _printf.c
+++ _printf.c
@@ -1,34 +1,51 @@
 #include "main.h"
 
 /**
- * _printf - implementation of the inbuilt printf
- * @format: the format specifier
- * Return: the formated string
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
  */
 
 int _printf(const char *format, ...)
 {
-	int printed = 0;
+	int i = 0, j = 0, buff_count = 0, prev_buff_count = 0;
+	char buffer[2000];
+	va_list arg;
+	call_t container[] = {
+		{'c', print_char}, {'s', print_str}, {'i', print_int}, {'d', print_int},
+		{'%', print_perc}, {'b', print_bin}, {'o', print_oct}, {'x', print_hex},
+		{'X', print_X}, {'u', print_uint}, {'R', print_R13}, {'r', print_rev},
+		{'\0', NULL}
+	};
 
-	va_list args;
-
-	va_start(args, format);
-
-	while (*format != '\0')
+	if (!format)
+		return (-1);
+	va_start(arg, format);
+	while (format && format[i] != '\0')
 	{
-		if (*format == '%')
+		if (format[i] == '%')
 		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
+			i++, prev_buff_count = buff_count;
+			for (j = 0; container[j].t != '\0'; j++)
+			{
+				if (format[i] == '\0')
+					break;
+				if (format[i] == container[j].t)
+				{
+					buff_count = container[j].f(buffer, arg, buff_count);
+					break;
+				}
+			}
+			if (buff_count == prev_buff_count && format[i])
+				i--, buffer[buff_count] = format[i], buff_count++;
 		}
 		else
-		{
-			_putchar(*format);
-			printed++;
-			format++;
-		}
+			buffer[buff_count] = format[i], buff_count++;
+		i++;
 	}
-	va_end(args);
-	return (printed);
+	va_end(arg);
+	buffer[buff_count] = '\0';
+	print_buff(buffer, buff_count);
+	return (buff_count);
 }
