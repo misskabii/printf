a8837994ed58de64a4e8d930c620e1136a536dd2
misskabii <shazmakesh254@gmail.com>
2023-03-28 23:34:01 +0300

    updated putchar

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,30 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - prints/writes character
- * @character: The character to print
- * Return: 1, success, -1 error
+ * _putchar - writes a char to stdout
+ * @character: character to print
+ * Return: Success(1), Error(1) sets errno
  */
-
 int _putchar(char character)
 {
-	return (write(1, &character, 1));
+	static int bufferCount;
+	static char buffer[1024];
+
+	if (character == -1)
+	{
+		bufferCount = 0;
+		return (0);
+	}
+	if (character == -2 || bufferCount == 1024)
+	{
+		write(1, buffer, bufferCount);
+		bufferCount = 0;
+	}
+	if (character != -1 && character != -2)
+	{
+		buffer[bufferCount] = character;
+		bufferCount++;
+		return (1);
+	}
+	return (0);
 }
