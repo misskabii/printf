58e73093694d2f68bc986b8fc1c7acb07a55fe04
misskabii <shazmakesh254@gmail.com>
2023-03-28 23:32:33 +0300

    printf.c

--- _printf.c
+++ _printf.c
@@ -1,34 +1,50 @@
 #include "main.h"
 
 /**
- * _printf - implementation of the inbuilt printf
- * @format: the format specifier
- * Return: the formated string
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
  */
-
-int _printf(const char *format, ...)
+int _printf(char *format, ...)
 {
-	int printed = 0;
-
+	int written = 0, (*print_fn)(char *, va_list);
+	char specifier[3];
 	va_list args;
 
+	if (format == NULL)
+		return (-1);
+	specifier[2] = '\0';
 	va_start(args, format);
-
-	while (*format != '\0')
+	_putchar(-1);
+	while (format[0])
 	{
-		if (*format == '%')
+		if (format[0] == '%')
 		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
+			print_fn = get_print_fn(format);
+			if (print_fn)
+			{
+				specifier[0] = '%';
+				specifier[1] = format[1];
+				written += print_fn(specifier, args);
+			}
+			else if (format[1] != '\0')
+			{
+				written += _putchar('%');
+				written += _putchar(format[1]);
+			}
+			else
+			{
+				written += _putchar('%');
+				break;
+			}
+			format += 2;
 		}
 		else
 		{
-			_putchar(*format);
-			printed++;
+			written += _putchar(format[0]);
 			format++;
 		}
 	}
-	va_end(args);
-	return (printed);
+	_putchar(-2);
+	return (written);
 }
